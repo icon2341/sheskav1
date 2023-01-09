238b5b53ca0ab4b44b1e519a5f9bd8c44b46ba3c
ashar rahman <asharrahman2002@gmail.com>
2023-01-09 07:32:59 -0500

    non grid dashboard, mobile responsive

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,36 +3,56 @@ import styles from './Dashboard.module.css'
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "../../index";
 import $ from "jquery"
-import Masonry from 'react-masonry-css'
+
 
 export function Dashboard(){
     console.log(auth.currentUser);
 
     const [user, loading, error] = useAuthState(auth);
-    const breakpointColumnsObj = {
-        default: 2,
-        1400: 1
-    };
 
 
     if(user) {
         return(
             <div>
                 <style> @import url('https://fonts.googleapis.com/css2?family=Pavanam&display=swap'); </style>
+                <style> @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap'); </style>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
                 <a id={styles["email-display"]}>{user?.email} Dashboard </a>
-                <Masonry
-                    breakpointCols={breakpointColumnsObj}
-                    className={styles.mymasonrygrid}
-                    columnClassName={styles.mymasonrygrid_column}>
-                        <div className={styles.sheskalistcard}>
-                            <a className={styles.cardTitle}>Sheska List</a>
-                            <a className={styles.cardSubTitle}>Create beautiful cards to showcase items your guests can donate to.  </a>
-                            <img src={require("../../images/weddingVenueExample.webp")}/>
-                        </div>
-                        <div className={styles.eventInformationCard}>Event Information</div>
-                        <div className={styles.guestListCard}>Guest List</div>
-                        <div className={styles.eventPreferencesCard}>Event Preferences</div>
-                </Masonry>
+                <div id={styles["features-grid"]}>
+                    <div className={styles.welcomeSpace}>
+                        <h1 id={styles["hello-message"]}> Welcome Ashar and Safin!</h1>
+                        <h2 id={styles["start-here-message"]}> Start Here.</h2>
+                    </div>
+                    <div className={styles.largeFeatureCard}>
+                        <a>Sheska List</a>
+                        <p>Create beautiful cards to showcase items your guests can donate to.</p>
+                        <img src={require("../../images/weddingVenueExample.webp")} id={styles["feature-card-image-sheska"]}/>
+                    </div>
+                    <div className={styles.smallFeatureCard}>
+                        <a>Event Information</a>
+                        <p>Create informative panels to guide your guests on your special day.</p>
+                        <svg className={styles.arrow} height="48" width="48"><path fill="white" d="m28.05 35.9-2.15-2.1 8.4-8.4H8v-3h26.3l-8.45-8.45 2.15-2.1L40.05 23.9Z"/></svg>
+                    </div>
+                    <div className={styles.largeFeatureCard}>
+                        <a>Guest List</a>
+                        <p>View and manage guests, dining, seating, and invitations.</p>
+                        <img src={require("../../images/weddingVenueExample.webp")} id={styles["feature-card-image-guest"]}/>
+                    </div>
+                    <div className={styles.simpleFeatureCard}>
+                        <a>Event Preferences</a>
+                        <p>Set up your event preferences and customize your event.</p>
+                    </div>
+                    <div className={styles.simpleFeatureCard}>
+                        <a>User Profile</a>
+                        <p>Mange your profile, accounting, and aesthetics.</p>
+                    </div>
+                    <div className={styles.simpleFeatureCard}>
+                        <a>Dashboard</a>
+                        <p>View your event analytics and manage your event.</p>
+                    </div>
+                </div>
+
+
             </div>
         )
     } else if(loading) {
