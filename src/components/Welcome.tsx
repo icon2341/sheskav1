b90e93e4ebac2f7475ebd308321dad0e2d0a35e7
ashar <asharrahman2002@gmail.com>
2023-02-13 13:08:46 -0500

    modifications to sign up page and welcome page

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,21 +1,20 @@
+import {Link, useNavigate} from "react-router-dom";
 import "./Welcome.css"
-import {Link} from "react-router-dom";
-
 //TODO add sign in functionality to event dashboard as well as sign in pages.
 
 export function Welcome() {
+    const navigate = useNavigate();
+
     return(
         <div>
             <span id={"page"}>
                 <h1 id={"welcome-logo"}>Sheska</h1>
                 <h2 id={"sub-text"}>Memories are made together</h2>
                 <div id={"button-box"}>
-                    <Link to="/signup">
-                        <button className={"button"}>Sign Up</button>
-                    </Link>
-                    <Link to={"/about"}>
-                        <button className={"button"}>Learn More</button>
-                    </Link>
+                    <button className={"button"} onClick={()=>{navigate('/signup')}}>
+                        Sign Up</button>
+                    <button className={"button"} onClick={()=>{navigate('/about')}}>
+                        Learn More</button>
                 </div>
                 <h2 id={"guest-info"}>Guest portal on the app</h2>
                 <h3 id={"download"}>Download Now!</h3>
