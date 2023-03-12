b40ce285f9da888f8f41a394ad1321a8fbc949be
ashar <asharrahman2002@gmail.com>
2023-03-12 04:20:07 -0400

    Basic DND image manger done

--- src/components/SheskaList/EditItem.tsx
+++ src/components/SheskaList/EditItem.tsx
@@ -30,11 +30,38 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { FilePondFile } from "filepond";
 import 'filepond/dist/filepond.min.css';
-import { deleteObject, ref, uploadBytes } from "firebase/storage";
+import {deleteObject, FirebaseStorage, getDownloadURL, listAll, ref, uploadBytes} from "firebase/storage";
 import Toast from "react-bootstrap/Toast";
 import "./NewItemUtil.scss";
 import {Swiper} from "swiper/react";
+import {getSheskaCardImagesUrls} from "../Utils/CardUtil";
+import {Auth} from "firebase/auth";
+import {string} from "yup";
+import {v4 as uuidv4} from "uuid";
 
+// import {DragDropContext, Droppable} from 'react-beautiful-dnd';
+import {DragDropContext} from "@hello-pangea/dnd";
+import {
+    DndContext,
+    closestCenter,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors, useDraggable, useDroppable,
+    DragOverlay,
+} from '@dnd-kit/core';
+import {
+    arrayMove, horizontalListSortingStrategy,
+    rectSwappingStrategy,
+    rectSortingStrategy,
+    SortableContext,
+    sortableKeyboardCoordinates, useSortable,
+    verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
+import {CSS} from '@dnd-kit/utilities';
+import zIndex from "@mui/material/styles/zIndex";
+import { Box } from "grommet";
 
 
 /**
@@ -49,13 +76,15 @@ export function EditItem() {
 
     //verify authentication
     const [user, loading, error] = useAuthState(auth);
-    const [images, setImages] = useState([]);
+    const [images, setImages] = useState<{ [id: string]: string }>();
     const navigate = useNavigate();
     const location = useLocation();
     const [docRef, setDocRef] = useState<DocumentReference>();
     const [title, setTitle] = React.useState(location.state.title);
     const [subtitle, setSubtitle] = React.useState(location.state.subtitle);
-    const [files, setFiles] = useState<FilePondFile[]>([])
+    const [files, setFiles] = useState<FilePondFile[]>([]);
+    const [imageOrder, setImageOrder] = useState<string[]>([]);
+
 
     const handleTitle = (event: ChangeEvent<HTMLInputElement>) => {
         setTitle(event.currentTarget.value);
@@ -111,27 +140,164 @@ export function EditItem() {
             `,
     })
 
+    useEffect(() => {
+        if(user) {
+            getSheskaCardImagesUrls(location.state.cardID, storage, auth).then((urls) => {
+                const imageMap: { [id: string]: string } = {};
+                const imageOrderInternal: string[] = [];
+                urls.forEach((url) => {
+                    let id = uuidv4();
+                    imageMap[id] = url;
+                    imageOrderInternal.push(id);
+
+                })
+                setImages(imageMap)
+                setImageOrder(imageOrderInternal);
+            })
+
+        }
+    }, [user])
+
+    useEffect(() => {
+        console.log("Images: " + images);
+        console.log("Image Order: " + imageOrder);
+    }, [images, imageOrder])
+
+    const [activeId, setActiveId] = useState(null);
+
+    function handleDragStart(event: any) {
+        setActiveId(event.active.id);
+    }
+
+    function handleDragEnd(event: any) {
+        const {active, over} = event;
+        setActiveId(null);
+        if (active.id !== over.id) {
+
+            setImageOrder((items : any) => {
+                const oldIndex = items.indexOf(active.id);
+                const newIndex = items.indexOf(over.id);
+
+                return arrayMove(items, oldIndex, newIndex);
+            });
+        }
+    }
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
+
+    //TODO integrate FILEPOND or some other DnD file uploader into this
+    //TODO add 'x' button to remove images from the list and from storage via Filepond API
+    //TODO add fileOrderFunctionality so that the user can reorder the images and it can show up elewhere in the list
+    //TODO add custom drag and drop UI
+    //TODO add error message and success message UI
+    //Abstract the DND image manager so that it can be used elsewhere
 
 
+    if(user) {
+        // @ts-ignore
+        return (
+            <div className={styles.pageContainer}>
+                <div className={styles.formContainer}>
+                    <h1 className={styles.title}>Edit Card</h1>
+                    <h2 className={styles.subtitle}>Title:</h2>
+                    <Form.Group className={"mb-3 w-75 mx-auto"} controlId="formBasicEmail">
+                        <Form.Control type="text" placeholder={location.state.title} onChange={handleTitle}/>
+                    </Form.Group>
 
-    // @ts-ignore
-    return (
-        <div className={styles.pageContainer}>
-            <div className={styles.formContainer}>
-                <h1 className={styles.title}>Edit Card</h1>
-                <h2 className={styles.subtitle}>Title:</h2>
-                <Form.Group className={"mb-3 w-75 mx-auto"} controlId="formBasicEmail">
-                    <Form.Control type="text" placeholder={location.state.title} onChange={handleTitle}/>
-                </Form.Group>
-                <Swiper allowTouchMove={false}>
+                    <DndContext
+                        sensors={sensors}
+                        collisionDetection={closestCenter}
+                        onDragEnd={handleDragEnd}
+                        onDragStart={handleDragStart}
+                    >
+                        <Box
+                            flex={true}
+                            wrap={true}
+                            direction="row"
+                            className={styles.imageContainer}
+                        >
 
-                </Swiper>
+                            <SortableContext items={imageOrder} strategy={rectSortingStrategy}>
+                                {imageOrder.map(id => <SortableItem key={id} id={id} images={images} />)}
+                            </SortableContext>
+                        </Box>
+                    </DndContext>
+
+
+                </div>
 
             </div>
-
+        )
+    } else {
+        return (
+        <div>
+            <h1>Not logged in</h1>
         </div>
-    )
+            )
+    }
 
 }
 
 export default EditItem;
+
+
+function Draggable(props: any) {
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: props.id,
+    });
+    const style = transform ? {
+        transform: CSS.Translate.toString(transform),
+    } : undefined;
+
+
+}
+
+function Droppable(props: any) {
+    const {isOver, setNodeRef} = useDroppable({
+        id: 'droppable',
+    });
+    const style = {
+        color: isOver ? 'green' : undefined,
+    };
+
+
+    return (
+        <div ref={setNodeRef} style={style}>
+            {props.children}
+        </div>
+    );
+}
+
+function SortableItem(props: any) {
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+    } = useSortable({id: props.id});
+
+    let style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        zIndex: 1000,
+    };
+
+    const imageStyle = {
+        width: '150px',
+        height: '100px',
+        borderRadius: '10px',
+        margin: '10px',
+    }
+
+    return (
+        <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+            <img style={imageStyle} src={props.images[props.id]} alt={'SUGMA'} className={styles.image}/>
+        </div>
+    );
+}
+
